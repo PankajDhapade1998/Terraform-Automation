7edc50b7b2ed3315f6de9d5b13a9a71e6df0fda4
Pankaj Dhapade <56977508+PankajDhapade1998@users.noreply.github.com>
2025-06-28 20:08:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-aws-devops-28"
+    bucket = "mydev-project-terraform-sample-batch-aws-devops-28-pankaj"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
